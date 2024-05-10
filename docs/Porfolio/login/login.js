cb6cf71bd259f0136e64ac1353fa6b8e7a73014e
LeonorMe <leonomedeiros@gmail.com>
2024-05-10 13:35:36 +0100

    trying to login js

--- docs/Porfolio/login/login.js
+++ docs/Porfolio/login/login.js
@@ -1,30 +1,60 @@
-const name = document.getElementById('name')
-const password = document.getElementById('password')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+const name = document.getElementById("name");
+const pass = document.getElementById("password");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
 
-form.addEventListener("submit", (e) =>{
-    let messages = []
-    if(name.value === '' || name.value == null){
-        messages.push('Name is required')
-    }
+console.log("Log in...");
 
-    if(password.length <= 6 || password.length >= 20){
-        messages.push('Password is to long or to short')
-    }
 
-    if(password.value === 'password'){
-        messages.push('Passwaord cannot be password')
-    }
+form.addEventListener("submit", (e) => {
+    console.log("Evaluating name and pass...");
+  let messages = [];
+  if (name.value === "" || name.value == null) {
+    messages.push("Name is required");
+  }
 
-    if(messages.length > 0){
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
-    }
+  if (pass.length <= 6 || pass.length >= 20) {
+    messages.push("Password is to long or to short");
+  }
 
-    if(name.value == "adminLM" && password.value == "0000"){
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join(", ");
+  }
+
+  /*
+    if(name.value == "" && password.value == ""){
         // go to restritec page
         window.location.replace("https://leonorme.github.io/portfolio/restricted/restrited_home.html"
         );
+    } */
+
+    console.log("Checking name to pass...");
+
+    var request = new XMLHttpRequest();
+    request.open("GET", "/docs/Porfolio/assets/data_base/users.xml", true);
+    request.send();
+    var xml = request.responseXML;
+    var users = xml.getElementsByTagName("user");
+
+  for (var i = 0; i < users.length; i++) {
+    var user = users[i];
+    var userChild = user.childNodes;
+    for (node in userChild) {
+      if (node.name == "username") {
+        var username = node.value;
+      }
+      if (NamedNodeMap.name == "password") {
+        var password = node.value;
+      }
     }
-})
+  }
+
+  if (username == name.vlaue && password == pass.value) {
+    console.log("Name and pass correct...");
+    // go to restritec page
+    window.location.replace(
+      //"https://leonorme.github.io/portfolio/restricted/restrited_home.html"
+    );
+  }
+});
