6166cdda0eb359eb84650f5ac3f6a63edbc4fba8
Leonor <fhs52247@fh-salzburg.ac.at>
2024-11-10 23:52:33 +0100

    music updates but dont play

--- docs/Music/getMusics.js
+++ docs/Music/getMusics.js
@@ -8,24 +8,34 @@ fetch("sounds.json")
 
 let MusicsList = document.getElementById("musics-list");
 
+function updateUrl(element) {
+    //console.log("updateUrl " + element.value);
+    let url = element.value;
+
+    document.getElementById("current-music").setAttribute("src", url);
+    //console.log("current-music " + document.getElementById("current-music").src);
+}
+
 function addDataToHTML() {
     musics.forEach((music) => {
-      console.log(music.name);
 
       let newMusic = document.createElement("div");
       //newMusic.href = "index.html?id=" + music.id;
       newMusic.classList.add("music");
       newMusic.classList.add("music-" + music.genre);
       newMusic.innerHTML = `
-                <img src="${music.cover}" alt="cover - ${music.name}">
-                <h3>${music.name}</h3>
-                <p>${music.artist}, ${music.album} ${music.year}</p>
-                <audio class="controls" src="${music.url}" loop>Audio Element Not Supported</audio>
-                <div>
-                    <button onclick="document.querySelector('.controls').play()">Play</button>
-                    <!--button onclick="document.querySelector('.controls').pause()">Pause</button-->
-                </div>
-                `;
+        <img src="${music.cover}" alt="cover - ${music.name}">
+        <h3>${music.name}</h3>
+        <p>${music.artist}, ${music.album} ${music.year}</p>
+        <button value="${music.url}" onclick="updateUrl(this)">Play</button>
+        `;
+      /*
+        <div>
+            <audio class="controls" src="${music.url}" loop>Audio Element Not Supported</audio>
+            <button onclick="document.querySelector('.controls').play()">Play</button>
+            <!--button onclick="document.querySelector('.controls').pause()">Pause</button-->
+        </div>
+        */
       // duration, lyrics, ...
       // TODO cor de fundo com genre
       // substituir controls por butao de play
