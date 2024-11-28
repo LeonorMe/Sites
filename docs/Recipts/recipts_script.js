92bbce6eaa39cc745b24e2cef91d4684146c2e5b
Leonor <53794672+LeonorMe@users.noreply.github.com>
2024-11-28 15:56:59 +0100

    Added base of recipts site

---
+++ docs/Recipts/recipts_script.js
@@ -0,0 +1,10 @@
+[
+    {
+        "id": 0,
+        "name": recipt0,
+        "calories": 0,
+        "ingridients": {a, b, c},
+        "nutrients": {A, B, C},
+        "steps": {aa, bb, cc}
+    }
+]
